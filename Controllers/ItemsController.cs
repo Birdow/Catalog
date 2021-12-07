305260aa32936656cfbe23620322a14068e66699
Robo <hikikomono@gmail.com>
2021-12-07 12:35:45 +0100

    Add Items Model
    Add InMemory "Database"
    
    Add Items Controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")] // GET /items
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemRepository();
+        }
+        
+        //GET  /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return repository.GetItems();
+        }
+    }
+}
---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item {Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow},
+            new Item {Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow},
+            new Item {Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 10, CreatedDate = DateTimeOffset.UtcNow},
+        };
+        
+        // Basic interface to return collection of items
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            // return 1 item or Default -> null
+            return items.SingleOrDefault(items => items.Id == id);
+            // return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
