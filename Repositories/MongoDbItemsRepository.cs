4d332a950e8b0be07624aa91a5267421ec53c4ce
Robo <hikikomono@gmail.com>
2021-12-09 17:38:24 +0100

    add mongo db implementation
    add Post, Get methods for new mongo DB implementation

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemRepository
+    {
+        private const string databaseName = "catalog";
+        private const string collectionName = "items";
+
+        private readonly  IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
+        }
+        
+        public IEnumerable<Item> GetItems()
+        {
+            return itemsCollection.Find(new BsonDocument()).ToList();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
+        }
+
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
