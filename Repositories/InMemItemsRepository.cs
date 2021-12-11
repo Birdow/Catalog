3230ce5e17831be18c35c4e9ed782ffa648c9034
Robo <hikikomono@gmail.com>
2021-12-11 14:27:43 +0100

    Remove bin

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Catalog.Entities;
 
 namespace Catalog.Repositories
@@ -16,33 +17,34 @@ namespace Catalog.Repositories
         };
         
         // Basic interface to return collection of items
-        public IEnumerable<Item> GetItems()
+        public async Task<IEnumerable<Item>> GetItemsAsync()
         {
-            return items;
+            return await Task.FromResult(items);
         }
 
-        public Item GetItem(Guid id)
+        public  async Task<Item> GetItemAsync(Guid id)
         {
-            // return 1 item or Default -> null
-            return items.SingleOrDefault(items => items.Id == id);
-            // return items.Where(item => item.Id == id).SingleOrDefault();
+            return await Task.FromResult(items.SingleOrDefault(items => items.Id == id));
         }
 
-        public void CreateItem(Item item)
+        public async Task CreateItemAsync(Item item)
         {
             items.Add(item);
+            await Task.CompletedTask;
         }
 
-        public void UpdateItem(Item item)
+        public async Task UpdateItemAsync(Item item)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
             items[index] = item;
+            await Task.CompletedTask;
         }
 
-        public void DeleteItem(Guid id)
+        public async Task DeleteItemAsync(Guid id)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == id);
             items.RemoveAt(index);
+            await Task.CompletedTask;
         }
     }
 }
